4f80da5685bb9930a6256feab4044141ffffac7b
Heli Sivunen <heli@mooncat.tv>
2019-09-24 13:51:16 +0100

    Add touch in and touch out methods.

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -3,19 +3,32 @@
 class Oystercard
   MAXIMUM_BALANCE = 90
 
-  attr_reader :balance
+  attr_reader :balance, :in_journey
 
   def initialize
     @balance = 0
+    @in_journey = false
   end
 
   def top_up(amount)
-    raise "Maximum balance of £90 exceeded" if amount + balance > MAXIMUM_BALANCE
+    raise 'Maximum balance of £90 exceeded' if amount + balance > MAXIMUM_BALANCE
 
     @balance += amount
     end
 
   def deduct(amount)
     @balance -= amount
+  end
+
+  def in_journey?
+    in_journey
+  end
+
+  def touch_in
+    @in_journey = true
+  end
+
+  def touch_out
+    @in_journey = false
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -7,6 +7,22 @@ describe Oystercard do
     expect(subject.balance).to eq(0)
   end
 
+  it 'is initially not in a journey' do
+    expect(subject).not_to be_in_journey
+  end
+
+  it 'can touch in' do
+    card = Oystercard.new
+    card.touch_in
+    expect(card).to be_in_journey
+  end
+
+  it 'can touch out' do
+    card = Oystercard.new
+    card.touch_out
+    expect(card).not_to be_in_journey
+  end
+
   describe '#top_up' do
     it { is_expected.to respond_to(:top_up).with(1).argument }
 
@@ -17,16 +33,23 @@ describe Oystercard do
     it 'raise an error if maximum balance is exceeded' do
       maximum_balance = Oystercard::MAXIMUM_BALANCE
       subject.top_up(maximum_balance)
-      expect { subject.top_up 1 }.to raise_error "Maximum balance of £90 exceeded"
-      end
+      expect { subject.top_up 1 }.to raise_error 'Maximum balance of £90 exceeded'
     end
+  end
 
   describe '#deduct' do
     it { is_expected.to respond_to(:deduct).with(1).argument }
 
     it 'can deduct money from the balance' do
-      expect { subject.deduct 1}.to change { subject.balance }.by -1
+      expect { subject.deduct 1 }.to change { subject.balance }.by (-1)
     end
   end
-
 end
+
+  # describe '#touch_in' do
+  #   it { is_expected.to respond_to :touch_in }
+  #
+  #   it 'is aware that it is in use' do
+  #     expect { subject.touch_in }.to raise 'Journey in progress'
+  #   end
+  # end
